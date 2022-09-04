7ab5c33670752909509098b47836d4553a8e1df6
Samokhin Alexander <icorwin74@gmail.com>
2022-09-04 22:06:43 +0300

    Add display list of intents

--- add_intent.py
+++ add_intent.py
@@ -10,9 +10,15 @@ project_id = env('PROJECT_ID')
 
 with open("questions.json", "r") as _:
     file_contents = _.read()
+print('\tВ файле содержатся следующие события:\n')
+for count, intent in enumerate(json.loads(file_contents).keys(), 1):
+    print(f'\t{count}. {intent}')
+'''
 intent_q_and_a = json.loads(file_contents)[INTENT]
 create_intent(
               project_id, INTENT,
               intent_q_and_a['questions'],
               [intent_q_and_a['answer']]
 )
+'''
+
